adad7c9cf2567783df221e643a517fdfe18c7b7c
Sophia See <sophiasee11@gmail.com>
2025-03-25 14:05:28 +0800

    finished coding challenge

---
+++ app/api/schedule-message/route.ts
@@ -0,0 +1,38 @@
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  const body = await request.text();
+
+  const data = JSON.parse(body)
+  console.log({ data })
+  const { url: webhookUrl, text, delay } = data;
+
+  // seconds to ms
+  const delayMs = delay * 1000;
+
+  setTimeout(async() => {
+    try {
+  
+      if (!webhookUrl || !text) {
+        return NextResponse.json({ message: 'Webhook error', error: "Missing required fields" })
+      }
+  
+      const response = await fetch(webhookUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          text: text,
+        }),
+      });
+  
+      console.log({ response })
+  
+      return NextResponse.json({ status: 200, data: response })
+    } catch (error: unknown) {
+      console.log({ error })
+      return NextResponse.json({ status: 404, error: "Error" })
+    }
+  }, delayMs ?? 0)
+}
---
+++ components/MessageForm.tsx
@@ -0,0 +1,153 @@
+"use client"
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import React from 'react'
+import { useForm } from 'react-hook-form'
+import { z } from "zod"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import { toast } from "sonner"
+
+const DelayUnit = ["seconds", "minutes", "hours"] as const;
+
+const formSchema = z.object({
+  delay: z.string(),
+  unit: z.enum(DelayUnit),
+  text: z.string().min(1, { message: "Must be at least 1 character" }),
+  url: z.string().min(1, { message: "Must be at least 1 character" }),
+})
+
+export default function MessageForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      delay: "0",
+      text: "",
+      url: "",
+      unit: "seconds"
+    },
+  })
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    const { url, text, delay, unit } = values;
+
+    if (!delay || parseFloat(delay) < 0){
+      toast("Invalid delay")
+    } else {
+      const delayNum = parseFloat(delay)
+      
+      const delayInSeconds = unit === "seconds"
+          ? delayNum
+          : unit === "minutes"
+          ? delayNum * 60
+          : delayNum * 3600;
+  
+      const response = await fetch("/api/schedule-message", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ url, text, delay:delayInSeconds }),
+      });
+  
+      if (response.status == 200) {
+        toast("Message sent!")  
+        form.reset();
+      } else {
+        toast.error("Message not sent.")
+      }
+
+    }
+
+  }
+  
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+      <div className='flex items-center gap-2'>
+        <FormField
+          control={form.control}
+          name="delay"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Delay</FormLabel>
+              <FormControl>
+              <Input 
+                {...field}
+                type="number" 
+              />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="unit"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Delay Unit</FormLabel>
+              <FormControl>
+                <Select value={field.value} onValueChange={field.onChange}>
+                  <SelectTrigger className="w-[180px]">
+                    <SelectValue placeholder="Delay Unit" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    {DelayUnit.map((delay) => (
+                      <SelectItem value={delay} key={delay}>
+                        {delay}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </div>
+
+        <FormField
+          control={form.control}
+          name="text"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Message</FormLabel>
+              <FormControl>
+                <Input placeholder="Enter your message" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="url"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Hook URL</FormLabel>
+              <FormControl>
+                <Input placeholder="Enter your hook url" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Send in </Button>
+      </form>
+    </Form>
+  )
+}
